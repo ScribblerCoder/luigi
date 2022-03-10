ffffe5ab26c09bcb3ee91fd426b3a568c6a961fb
luigi <luigi@localhost.localdomain>
2022-03-10 23:50:46 +0300

    Added Rots

--- LowEffortBuster.py
+++ LowEffortBuster.py
@@ -1,7 +1,11 @@
 import sys
+import codecs
+import base64
+import string
 
-def help(): 
-	asciiart = """
+
+def help():
+    asciiart = """
 █████                               ██████████  ██████ ██████                   █████   
 ░░███                               ░░███░░░░░█ ███░░█████░░███                 ░░███    
  ░███         ██████  █████ ███ █████░███  █ ░ ░███ ░░░███ ░░░██████  ████████  ███████  
@@ -18,26 +22,54 @@ def help():
       ░███    ░███░███ ░███ ░░░░███░███ ███░███░░░   ░███                                
       ███████████ ░░██████████████ ░░█████ ░░██████  █████                               
      ░░░░░░░░░░░   ░░░░░░░░░░░░░░   ░░░░░   ░░░░░░  ░░░░░  luigi  v1.0.0"""
-	print(asciiart)
-	print("\n\nusage: python3 LowEffortBuster.py <file> <flag_format>")
-
-Flag_Format = ""
-if len(sys.argv) != 2:
-	help()
-	exit() 
-	
-
-f = open(sys.argv[1],'r')
-flag_format = sys.argv[2]
+    print(asciiart)
+    print("\n\nusage: python3 LowEffortBuster.py <file>")
 
 
+def rot(flag, key):
+
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    result = ""
+    # transverse the plain text
+    for i in range(len(flag)):
+        char = flag[i]
+    # Encrypt uppercase characters in plain text
+
+    if char in upper:
+        result += chr((ord(char) + key - 65) % 26 + 65)
+    # Encrypt lowercase characters in plain text
+    else:
+        result += chr((ord(char) + key - 97) % 26 + 97)
+    return result
 
 
+def base64(flag):
+    cipher = base64.b64encode(flag)
+    return cipher
 
+
+def main():
+
+    Flag_Format = ""
+    ciphers = []
+    if len(sys.argv) != 2:
+        help()
+        exit()
+
+    HexDump_file = open(sys.argv[1], "r").read()
+
+    for i in range(0, 26):
+        tmp = rot(Flag_Format, i)
+        tmp = tmp.encode()
+        ciphers.append(tmp.hex())
+
+
+main()
 
 # encodings to add
 # - ROT13 (and all possible ROTs)
 # - Base64 ( and hex/decimal/octal/binary/base32/base85)
 # - URL encode
 # - braille
-# - reverse 
+# - reverse
